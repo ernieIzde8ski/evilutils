c244f8a02b1ed8a9895fc7443ef0c8d42f9d9159
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2025-02-02 18:21:05 -0500

    feat: add eviltyping
    I had a much longer commit, but I realized that it was bullshit.
    Eviltyping is still nice though.

--- src/evilfunctools/__init__.py
+++ src/evilfunctools/__init__.py
@@ -6,11 +6,11 @@ from .compose import compose
 from .decorators import immediate
 
 __all__ = [
-    "partial",
-    "partialmethod",
     "cache",
     "cached_property",
     "compose",
-    "wraps",
     "immediate",
+    "partial",
+    "partialmethod",
+    "wraps",
 ]
---
+++ src/eviltyping/__init__.py
@@ -0,0 +1,33 @@
+"""All the types I don't want to write twice."""
+
+from os import PathLike
+from typing import TYPE_CHECKING, Any, Protocol, TypeVar
+
+__all__ = ["StrPath"]
+
+type StrPath = str | PathLike[str]
+"""Like StrPath from typeshed."""
+
+
+class Callable[**P, RT](Protocol):
+    """Like typing.Protocol but it doesn't scream at me for being wrong."""
+
+    def __call__(self, *args: P.args, **kwds: P.kwargs) -> RT: ...
+
+
+class Newable[**P, RT](Protocol):
+    def __new__(cls, *args: P.args, **kwargs: P.kwargs) -> RT: ...
+
+
+if TYPE_CHECKING:
+
+    def strong_cast[T](
+        _type: type[T] | TypeVar,
+        _value: Any,  # pyright: ignore[reportAny, reportExplicitAny]
+        /,
+    ) -> T: ...
+
+else:
+
+    def strong_cast[T](_type, _value):
+        return _value
