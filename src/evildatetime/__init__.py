1cec51994a66f6d398ebe550c4fce04926f8cb0b
Ernest Izdebski <izdebski@andrews.edu>
2026-04-21 13:35:19 -0400

    evildatetime: Add aliases for remaining builtin `datetime` props

--- src/evildatetime/__init__.py
+++ src/evildatetime/__init__.py
@@ -1,5 +1,8 @@
-__all__ = ["Date", "DateTime", "Time"]
+__all__ = ["Date", "DateTime", "Time", "TimeDelta", "TimeZone", "TzInfo"]
 
 from datetime import date as Date
 from datetime import datetime as DateTime
 from datetime import time as Time
+from datetime import timedelta as TimeDelta
+from datetime import timezone as TimeZone
+from datetime import tzinfo as TzInfo
